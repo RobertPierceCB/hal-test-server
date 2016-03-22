124f357172852a612660d40a34cf4c485f43ddf9
Robert Pierce <ropierce@surfacebook.cb.careerbuilder.com careerbuilder.com>
2016-03-22 09:21:28 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+func halResponse(c web.C, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+
+	testResponse, _ := ioutil.ReadFile("fixtures/tunnelResponse.json")
+
+	fmt.Fprintln(w, string(testResponse))
+}
+
+func main() {
+	goji.Get("/*", halResponse)
+	goji.Serve()
+}
